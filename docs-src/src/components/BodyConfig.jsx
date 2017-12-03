ba5d69b3914f0528abdeb2aa3148f02992f68fd0
Minyoung Choi <minyoung.choi@gmail.com>
2017-12-03 13:45:51 -0800

    move config defaults to reducer; populate config state from store and update store on state change

--- docs-src/src/components/BodyConfig.jsx
+++ docs-src/src/components/BodyConfig.jsx
@@ -91,50 +91,8 @@ const styles = () => ({
 })
 
 class BodyConfig extends React.Component {
-  state = {
-    generalProgram: 'Chrome',
-    generalJSTOffset: '0',
-    scheduledSleepEnabled: true,
-    scheduledSleepStart: new Date(new Date().setHours(0, 0, 0, 0)),
-    scheduledSleepLength: '4',
-    expeditionsEnabled: false,
-    expeditionsFleet2Enabled: true,
-    expeditionsFleet3Enabled: true,
-    expeditionsFleet4Enabled: true,
-    expeditionsFleet2: '2',
-    expeditionsFleet3: '5',
-    expeditionsFleet4: '38',
-    pvpEnabled: false,
-    combatEnabled: false,
-    combatEngine: 'legacy',
-    combatMap: '1-1',
-    combatFleetMode: '',
-    combatCombatNodes: null,
-    combatNodeSelect1: null,
-    combatNodeSelect2: null,
-    combatNodeSelects: null,
-    combatFormationsNode: null,
-    combatFormationsFormation: null,
-    combatFormations: null,
-    combatNightBattlesNode: null,
-    combatNightBattlesMode: null,
-    combatNightBattles: null,
-    combatRetreatLimit: 'heavy',
-    combatRepairLimit: 'moderate',
-    combatRepairTimeLimit: new Date(new Date().setHours(0, 30, 0, 0)),
-    combatLBASGroups: null,
-    combatLBASGroup1Node1: null,
-    combatLBASGroup1Node2: null,
-    combatLBASGroup2Node1: null,
-    combatLBASGroup2Node2: null,
-    combatLBASGroup3Node1: null,
-    combatLBASGroup3Node2: null,
-    combatOptionCheckFatigue: false,
-    combatOptionReserveDocks: false,
-    combatOptionPortCheck: false,
-    combatOptionMedalStop: false,
-    questsEnabled: true,
-  }
+  // grab default states from the store; defaults are in reducers/config/config.jsx
+  state = this.props.config.jsonConfig
 
   componentDidMount = () => {
     // this.props.setJsonConfig(this.state)
@@ -143,7 +101,7 @@ class BodyConfig extends React.Component {
 
   componentDidUpdate = (nextProp, nextState) => {
     if (this.state !== nextState) {
-      // this.props.setJsonConfig(this.state)
+      this.props.setJsonConfig(this.state)
       this.props.setPythonConfig(this.state)
     }
   }
