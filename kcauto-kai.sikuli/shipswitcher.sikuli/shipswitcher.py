7bff6ab2020ad99c796034c924d9b9b47d71fadf
Minyoung Choi <minyoung.choi@gmail.com>
2017-12-11 00:24:10 -0800

    some additional logic

--- kcauto-kai.sikuli/shipswitcher.sikuli/shipswitcher.py
+++ kcauto-kai.sikuli/shipswitcher.sikuli/shipswitcher.py
@@ -18,17 +18,21 @@ from globals import Globals
 
 
 class ShipSwitcher(object):
-    def __init__(self, config, stats, regions):
+    def __init__(self, config, stats, regions, fleets, combat):
         self.config = config
         self.stats = stats
         self.regions = regions
         self.kc_region = regions['game']
+        self.ship_count = 1
+        self.current_shiplist_page = 1
 
     def ship_switch_logic(self):
+        self._get_ship_count()
         for i in range(1, 7):
             if self._check_need_to_switch_ship(i):
                 self._press_switch_ship_button(i)
-                self._resolve_replacement_ship()
+                for '__OPTIONS':
+                    self._resolve_replacement_ship()
 
     def _get_ship_count(self):
         # read number at top of interface
@@ -58,5 +62,25 @@ class ShipSwitcher(object):
         elif target == 'start':
             pass
 
-    def _resolve_replacement_ship(self):
+    def _switch_shiplist_page(self, target_page):
+        current_page = self.curent_shiplist_page
         pass
+
+    def _choose_ship_by_position(self, position):
+        pass
+
+    def _check_ship_availability(self):
+        ship_not_available = False
+        if ship_not_available:
+            return False
+        return True
+
+    def _resolve_replacement_ship(self):
+        self._switch_shiplist_sorting('__TARGET')
+        self._switch_shiplist_page(5)
+        self._choose_ship_by_position(4)
+        if self._check_ship_availability():
+            # click switch button
+            return True
+        else:
+            return False
