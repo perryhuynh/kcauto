5c862469c398e1a8d8b29fd68fa020348eaf5f80
Minyoung Choi <minyoung.choi@gmail.com>
2017-12-17 00:44:01 -0800

    add dropdown

--- docs-src/src/components/Menu.jsx
+++ docs-src/src/components/Menu.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from 'material-ui/styles'
 import PropTypes from 'prop-types'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
+import MuiMenu, { MenuItem } from 'material-ui/Menu'
 import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
 import { Ferry, Earth, GithubCircle, Discord } from 'mdi-material-ui'
@@ -26,33 +27,69 @@ const styles = () => ({
   },
 })
 
-const Menu = (props) => {
-  const {
-    classes,
-    ui,
-    setUILanguage,
-  } = props
-  return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography type='title' color='inherit' className={classes.flex}>
-            <Ferry /> kcauto-kai
-          </Typography>
+class Menu extends Component {
+  state = {
+    languageDropdownAnchor: null,
+  }
 
-          <IconButton color='contrast' className={classes.langaugeButton}>
-            <Earth className={classes.largeIcon} />{ui.language.toUpperCase()}
-          </IconButton>
-          <a href={urls.DISCORD_LINK}>
-            <IconButton color='contrast'><Discord className={classes.largeIcon} /></IconButton>
-          </a>
-          <a href={urls.GITHUB_LINK}>
-            <IconButton color='contrast'><GithubCircle className={classes.largeIcon} /></IconButton>
-          </a>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
+  handleChangeLanguage = (language) => {
+    this.props.setUILanguage(language)
+    this.setState({ languageDropdownAnchor: null })
+  }
+  render = () => {
+    const {
+      classes,
+      ui,
+    } = this.props
+    const {
+      languageDropdownAnchor,
+    } = this.state
+    const dropdownOpen = Boolean(languageDropdownAnchor)
+    return (
+      <div className={classes.root}>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography type='title' color='inherit' className={classes.flex}>
+              <Ferry /> kcauto-kai
+            </Typography>
+
+            <IconButton
+              color='contrast'
+              className={classes.langaugeButton}
+              onClick={event => this.setState({ languageDropdownAnchor: event.currentTarget })}
+            >
+              <Earth className={classes.largeIcon} /> {ui.language.toUpperCase()}
+            </IconButton>
+            <MuiMenu
+              id='languageDropdown'
+              anchorEl={languageDropdownAnchor}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={dropdownOpen}
+              onRequestClose={() => this.setState({ languageDropdownAnchor: null })}
+            >
+              <MenuItem onClick={() => this.handleChangeLanguage('en')}>EN</MenuItem>
+              <MenuItem onClick={() => this.handleChangeLanguage('kr')}>KR</MenuItem>
+            </MuiMenu>
+
+
+            <a href={urls.DISCORD_LINK}>
+              <IconButton color='contrast'><Discord className={classes.largeIcon} /></IconButton>
+            </a>
+            <a href={urls.GITHUB_LINK}>
+              <IconButton color='contrast'><GithubCircle className={classes.largeIcon} /></IconButton>
+            </a>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
 }
 
 Menu.propTypes = {
