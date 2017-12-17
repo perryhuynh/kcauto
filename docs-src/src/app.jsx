a3286a36f1277f83e8d56eaa76e11c8455510f3c
Minyoung Choi <minyoung.choi@gmail.com>
2017-12-17 00:26:11 -0800

    begin adding localization files; begin adding dropdown

--- docs-src/src/app.jsx
+++ docs-src/src/app.jsx
@@ -11,7 +11,7 @@ import Grid from 'material-ui/Grid'
 import { getUIMode } from 'actions/ui/mode'
 import { setUILanguage } from 'actions/ui/language'
 import Reducers from 'reducers/'
-import Menu from 'components/Menu'
+import MenuContainer from 'containers/MenuContainer'
 import Body from 'components/Body'
 import Footer from 'components/Footer'
 
@@ -24,12 +24,12 @@ const reduxMiddleware = process.env.NODE_ENV === 'production' ?
 store = compose(reduxMiddleware)(createStore)(store)
 
 store.dispatch(getUIMode())
-store.dispatch(setUILanguage('english'))
+store.dispatch(setUILanguage('en'))
 
 render(
   <Provider store={store}>
     <Grid container>
-      <Menu />
+      <MenuContainer />
       <Body />
       <Footer />
     </Grid>
--- docs-src/src/components/Menu.jsx
+++ docs-src/src/components/Menu.jsx
@@ -5,7 +5,7 @@ import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
-import { Ferry, GithubCircle, Discord } from 'mdi-material-ui'
+import { Ferry, Earth, GithubCircle, Discord } from 'mdi-material-ui'
 
 import * as urls from 'urls'
 
@@ -16,6 +16,10 @@ const styles = () => ({
   flex: {
     flex: 1,
   },
+  langaugeButton: {
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: '14px',
+  },
   largeIcon: {
     width: 30,
     height: 30,
@@ -23,7 +27,11 @@ const styles = () => ({
 })
 
 const Menu = (props) => {
-  const { classes } = props
+  const {
+    classes,
+    ui,
+    setUILanguage,
+  } = props
   return (
     <div className={classes.root}>
       <AppBar position='static'>
@@ -31,6 +39,10 @@ const Menu = (props) => {
           <Typography type='title' color='inherit' className={classes.flex}>
             <Ferry /> kcauto-kai
           </Typography>
+
+          <IconButton color='contrast' className={classes.langaugeButton}>
+            <Earth className={classes.largeIcon} />{ui.language.toUpperCase()}
+          </IconButton>
           <a href={urls.DISCORD_LINK}>
             <IconButton color='contrast'><Discord className={classes.largeIcon} /></IconButton>
           </a>
@@ -45,6 +57,8 @@ const Menu = (props) => {
 
 Menu.propTypes = {
   classes: PropTypes.object.isRequired,
+  ui: PropTypes.object.isRequired,
+  setUILanguage: PropTypes.func.isRequired,
 }
 
 export default withStyles(styles)(Menu)
