de988767d046991359b90504e1b74d25ba83ff59
Minyoung Choi <minyoung.choi@gmail.com>
2018-02-24 11:36:24 -0800

    additional checks and stops in shipswitcher-by-ship and sparkle; updates to combat module disabled

--- kcauto-kai.sikuli/shipswitcher.sikuli/shipswitcher.py
+++ kcauto-kai.sikuli/shipswitcher.sikuli/shipswitcher.py
@@ -514,16 +514,24 @@ class ShipSwitcher(object):
         Returns:
             bool: True if a successful switch was made; False otherwise
         """
-        for ship in slot_config['ships']:
+        for ship in list(slot_config['ships']):
             self._switch_shiplist_sorting(ship['sort_order'])
             if 'offset_ref' in ship and 'offset' in ship:
                 page, positions = self._resolve_ship_page_and_position(
                     ship['offset_ref'], ship['offset'])
                 self._navigate_to_shiplist_page(page)
+            if 'sparkle' in slot_config['criteria']:
+                # if in sparkle mode, remove the checked ship from the list of
+                # possible ships so we don't go back to it
+                slot_config['ships'].pop(0)
             # there should only be one returned position
             if self._choose_and_check_availability_of_ship(
                     positions[0], slot_config['criteria']) is True:
                 return True
+        if 'sparkle' in slot_config['criteria']:
+            # if in sparkle mode and we reach this point, we've exhausted the
+            # list of possible ships; disable the combat module
+            self.combat.disable_combat_module()
         return False
 
     def _resolve_replacement_ship_by_asset(self, mode, slot_config):
