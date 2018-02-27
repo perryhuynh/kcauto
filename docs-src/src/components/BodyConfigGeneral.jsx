16dd6e29962eb3bdaac3ac0dd4549c2b6ec7365d
Minyoung Choi <minyoung.choi@gmail.com>
2018-02-26 23:00:07 -0800

    webui: add pause config support, default combat engine to live

--- docs-src/src/components/BodyConfigGeneral.jsx
+++ docs-src/src/components/BodyConfigGeneral.jsx
@@ -2,13 +2,21 @@ import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 
+import Select from 'react-select'
 import Grid from 'material-ui/Grid'
 import Typography from 'material-ui/Typography'
 import TextField from 'material-ui/TextField'
+import { InputLabel } from 'material-ui/Input'
+import { FormControl } from 'material-ui/Form'
 
 import Localize from 'containers/LocalizeContainer'
 import { styles } from 'components/BodyConfigStyles'
 
+
+const PAUSE_OPTIONS = [
+  { value: true, label: <Localize field='bodyConfig.generalPauseTrue' /> },
+  { value: false, label: <Localize field='bodyConfig.generalPauseFalse' /> },
+]
 
 class BodyConfigGeneral extends PureComponent {
   state = this.props.config
@@ -26,13 +34,14 @@ class BodyConfigGeneral extends PureComponent {
     const {
       generalProgram,
       generalJSTOffset,
+      generalPause,
     } = this.state
     return (
       <Fragment>
         <Typography variant='display1'><Localize field='bodyConfig.generalHeader' /></Typography>
 
         <Grid container spacing={0}>
-          <Grid item xs={12} sm={8} className={classes.formGrid}>
+          <Grid item xs={12} sm={4} className={classes.formGrid}>
             <TextField
               id='generalProgram'
               label={<Localize field='bodyConfig.generalProgram' />}
@@ -56,6 +65,23 @@ class BodyConfigGeneral extends PureComponent {
               fullWidth
               type='number'
               margin='normal' />
+          </Grid>
+          <Grid item xs={12} sm={4} className={classes.formGrid}>
+            <FormControl margin='normal' fullWidth>
+              <InputLabel htmlFor='generalPause' shrink={true} className={classes.reactSelectLabel}>
+                <Localize field='bodyConfig.generalPause' />
+              </InputLabel>
+              <Select
+                className={classes.reactSelect}
+                simpleValue={true}
+                name='generalPause'
+                value={generalPause}
+                options={PAUSE_OPTIONS}
+                clearable={false}
+                onChange={
+                  value => this.setState({ generalPause: value }, () => this.props.callback(this.state))}
+                fullWidth />
+            </FormControl>
           </Grid>
         </Grid>
       </Fragment>
