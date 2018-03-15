85690546b3f53927fb7c193abb531bf37b22e2a4
Minyoung Choi <minyoung.choi@gmail.com>
2018-03-14 23:03:51 -0700

    longer waits between lbas node assignments

--- kcauto-kai.sikuli/combat.sikuli/lbas.py
+++ kcauto-kai.sikuli/combat.sikuli/lbas.py
@@ -57,10 +57,11 @@ class LBAS(object):
                     self.kc_region.hover('lbas_panel_side.png')
                 node_obj.click_node(self.kc_region)
                 Util.rejigger_mouse(self.regions, 'lbas')
-                Util.kc_sleep()
+                Util.kc_sleep(1)
             Util.check_and_click(
                 self.regions['upper'], 'lbas_assign_confirm.png')
             Util.rejigger_mouse(self.regions, 'lbas')
+            Util.kc_sleep(2)
 
     def resupply_groups(self, check_fatigue):
         """Method for resupplying the LBAS groups. If check_fatigue is set to
