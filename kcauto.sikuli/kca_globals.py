59991b93ebfc29a191e4d6ec10d3bc88e3c181a4
Minyoung Choi <minyoung.choi@gmail.com>
2018-09-11 23:57:14 -0700

    revise click areas for prev and next scroll buttons

--- kcauto.sikuli/kca_globals.py
+++ kcauto.sikuli/kca_globals.py
@@ -40,7 +40,5 @@ class Globals(object):
     EXPAND = {
         'repair_list': [-500, 20, -12, 12],
         'arrow_navigation': [-10, 10, -8, 8],
-        'shiplist_sort': [-4, 36, -10, 10],
-        'scroll_prev': [-14, 20, -20, 5],
-        'scroll_next': [-20, 2, -14, 14]
+        'shiplist_sort': [-4, 36, -10, 10]
     }
