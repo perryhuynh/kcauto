ba80b60abd9b3f7bca4d658914f7159746441b1f
Minyoung Choi <minyoung.choi@gmail.com>
2018-09-22 15:53:55 -0700

    smaller search area

--- kcauto.sikuli/combat.sikuli/lbas.py
+++ kcauto.sikuli/combat.sikuli/lbas.py
@@ -106,7 +106,7 @@ class LBAS(object):
             if Util.check_and_click(
                     self.regions['right'], 'lbas_resupply.png'):
                 Util.log_msg("Resupplying LBAS group #{}.".format(group))
-                self.regions['right'].waitVanish(
+                self.regions['upper_right'].waitVanish(
                     'lbas_resupply_in_progress.png', 10)
             if check_fatigue:
                 Util.kc_sleep(1)
