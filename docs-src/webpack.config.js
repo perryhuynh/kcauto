4470d6f066e9f7a08dc2bae4f4d420bac69d1569
Minyoung Choi <minyoung.choi@gmail.com>
2018-12-25 23:46:48 -0800

    stage for master

--- docs-src/webpack.config.js
+++ docs-src/webpack.config.js
@@ -1,28 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const srcPath = path.join(__dirname, 'src');
 const modulesPath = path.join(__dirname, '..', 'node_modules');
 const outputPath = path.join(__dirname, '..', 'docs');
 
-module.exports = (env, argv) => {
+module.exports = env => {
   const pluginsArray = [new webpack.DefinePlugin({
     'process.version': JSON.stringify(process.env.npm_package_version),
   })];
-  if (argv.mode === 'production') {
-    pluginsArray.push(new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }))
-    pluginsArray.push(new webpack.optimize.UglifyJsPlugin());
-}
 
-  return {
+  const config = {
     entry: srcPath + '/index.jsx',
     output: {
       path: outputPath,
-      filename: 'bundle.js',
-      sourceMapFilename: '[file].map?[contenthash]'
+      filename: 'bundle.js'
     },
     plugins: pluginsArray,
     module: {
@@ -39,13 +31,28 @@ module.exports = (env, argv) => {
             'style-loader',
             'css-loader'
           ]
-        },
+        }
       ]
     },
     resolve: {
       modules: [srcPath, modulesPath],
       extensions: ['.js', '.jsx', '.css']
     },
-    devtool: 'source-map'
   }
+
+  if (env.NODE_ENV === 'production') {
+    config.optimization = {
+      minimizer: [
+        new UglifyJsPlugin({
+          parallel: true,
+          sourceMap: false,
+        })
+      ]
+    }
+  } else {
+    config.output.sourceMapFilename = '[file].map?[contenthash]'
+    config.devtool = 'source-map'
+  }
+
+  return config
 };
